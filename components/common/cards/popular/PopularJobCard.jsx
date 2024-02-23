8e526319b5b4a936c561bbd8b7e1cf557bdef1dd
n0dy <bata.dino00@gmail.com>
2024-02-24 00:42:08 +0100

    Initial commit

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,21 @@
+import { 
+  View, 
+  Text,
+  TouchableOpacity,
+  Image
+} from 'react-native';
+
+import styles from './popularjobcard.style';
+
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  return (
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      
+    </TouchableOpacity>
+  )
+}
+
+export default PopularJobCard
